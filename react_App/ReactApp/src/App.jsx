6ebb917ff1b84e5ddeaeb28f442937fe4cc6ec5e
Ankit-lama <tamangankit49@gmail.com>
2026-07-02 19:13:58 +0530

    new changes

--- react_App/ReactApp/src/App.jsx
+++ react_App/ReactApp/src/App.jsx
@@ -1,10 +1,21 @@
 import List from './List.jsx';
 
 function App() {
+    const record = [
+        { id: 1, name: 'React', calories: 100 },
+        { id: 2, name: 'Angular', calories: 150 },
+        { id: 3, name: 'Vue', calories: 120 }
+    ];
+
+    const vegetables = [
+        { id: 1, name: 'Carrot', calories: 41 },
+        { id: 2, name: 'Broccoli', calories: 55 },
+        { id: 3, name: 'Spinach', calories: 23 }
+    ];
     return (
         <div>
-            <h1>My App</h1>
-            <List />
+            <List items={record} category="Programming Languages" />
+            <List items={vegetables} category="Vegetables" />
         </div>
     );
 }
--- react_App/ReactApp/src/List.jsx
+++ react_App/ReactApp/src/List.jsx
@@ -1,8 +1,14 @@
+import './index.css';
 
-
-function List() {
-    const record = ['React', 'Angular', 'Vue'];
-    const ListItems = record.map((item) => <li>{item}</li>)
-    return(<ol>{ListItems}</ol>);
+function List(props) {
+    const record = props.items;
+    const category = props.category;
+    const ListItems = record.map((item) => <li key ={item.id}>{item.name}: &nbsp;
+                                                    <b>{item.calories}</b></li>)
+    
+    return(<>
+    <h3 className="list-title">{category}</h3>
+    <ol className="list-group">{ListItems}</ol>
+    </>);
 }
 export default List;
