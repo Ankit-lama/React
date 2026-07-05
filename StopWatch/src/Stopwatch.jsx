f8694ad1f7d88c00cfc132f3764273c3529ccfe7
Ankit-lama <tamangankit49@gmail.com>
2026-07-05 10:06:06 +0530

    New Changes

--- StopWatch/src/Stopwatch.jsx
+++ StopWatch/src/Stopwatch.jsx
@@ -8,20 +8,39 @@ function Stopwatch() {
     const startTimeRef = useRef(0);
 
     useEffect(()=>{
+        if(isRunning)
+        {
+            intervalIdRef.current = setInterval(() => {
+            setElapsedTime(Date.now() - startTimeRef.current);
+        }, 10);
+        }
+
+        return () => clearInterval(intervalIdRef.current);
 
     }, [isRunning]);
 
     function start() {
-
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
     }
     function stop() {
-        
+        setIsRunning(false);  
     }
     function reset() {
-        
+         setIsRunning(false);
+        setElapsedTime(0);
     }
     function formateTime() {
-        return `00:00:00`
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+        let seconds = Math.floor((elapsedTime / 1000) % 60);
+        let millisecond = Math.floor((elapsedTime) % 1000 /10);
+
+        hours =String(hours).padStart(2,'0');
+        minutes =String(minutes).padStart(2,'0');
+        seconds =String(seconds).padStart(2,'0');
+        millisecond =String(millisecond).padStart(2,'0');
+    return `${minutes}:${seconds}:${millisecond}`;
     }
 
     return(
